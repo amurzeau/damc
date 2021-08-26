66f846a31e3f0d05fe5aef519a5b4f6f2ed506d6
Alexis Murzeau <amubtdx@gmail.com>
2021-08-26 19:47:04 +0200

    OscArray/OscContainerArray: remove add/remove stuff which is unused

--- common/Osc/OscArray.h
+++ common/Osc/OscArray.h
@@ -10,9 +10,6 @@ public:
 	void setOscConverters(std::function<T(T)> convertToOsc, std::function<T(T)> convertFromOsc);
 	void setChangeCallback(std::function<void(T)> onChange) { this->onChange = onChange; }
 
-	void setAddCallback(std::function<void(T)> onAdd) { this->onAdd = onAdd; }
-	void setRemoveCallback(std::function<void()> onRemove) { this->onRemove = onRemove; }
-
 protected:
 	void initializeItem(OscVariable<T>* item) override;
 
@@ -20,25 +17,10 @@ private:
 	std::function<T(T)> convertToOsc;
 	std::function<T(T)> convertFromOsc;
 	std::function<void(T)> onChange;
-
-	std::function<void(T)> onAdd;
-	std::function<void()> onRemove;
 };
 
 template<typename T>
-OscArray<T>::OscArray(OscContainer* parent, std::string name) : OscGenericArray<OscVariable<T>>(parent, name) {
-	this->oscAddEndpoint.setCallback([this](auto&&) {
-		printf("Adding item to %s\n", this->getFullAddress().c_str());
-		T v{};
-		this->push_back(v);
-		this->onAdd(v);
-	});
-	this->oscRemoveEndpoint.setCallback([this](auto&&) {
-		printf("Removing last item from %s\n", this->getFullAddress().c_str());
-		this->onRemove();
-		this->pop_back();
-	});
-}
+OscArray<T>::OscArray(OscContainer* parent, std::string name) : OscGenericArray<OscVariable<T>>(parent, name) {}
 
 template<typename T>
 void OscArray<T>::setOscConverters(std::function<T(T)> convertToOsc, std::function<T(T)> convertFromOsc) {
